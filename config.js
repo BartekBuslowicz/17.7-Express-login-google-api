331e18efe16b30f17479561ddcfc16a69a60418d
Bartek Buslowicz <bartek@burin.pl>
2018-02-12 14:43:33 +0100

    Add  final project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '694077440319-479q6ls9mtm11tms151d2baquf9jkiu2.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'UO1TSSemgnTnklvGNISiZCzh',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
